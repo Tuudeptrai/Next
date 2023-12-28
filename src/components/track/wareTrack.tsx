c2f5bc88603e2409573477a35575b5ac29715e12
Vu Van Tuu <vuvantuu.gl@gmail.com>
2023-12-28 07:37:29 +0700

    waresurfer hook detail play pause

--- src/components/track/wareTrack.tsx
+++ src/components/track/wareTrack.tsx
@@ -1,11 +1,12 @@
 'use client'
 // Import WaveSurfer
 import WaveSurfer from 'wavesurfer.js'
-import React, { useEffect, useRef, useState ,useMemo} from 'react';
+import React, { useEffect, useRef, useState ,useMemo, useCallback} from 'react';
 import { useSearchParams } from 'next/navigation';
 import { WaveSurferOptions } from 'wavesurfer.js';
 // WaveSurfer hook
 import { useWavesurfer } from '@/utils/customHook';
+import { Button } from '@mui/material';
 
 
 
@@ -13,20 +14,46 @@ const WareTrack = () => {
     const searchParams = useSearchParams();
     const fileName = searchParams.get("audio");
     const containerRef = useRef<HTMLDivElement>(null);
-    const optionsMemo = useMemo(()=>{
+    const optionsMemo = useMemo(():Omit<WaveSurferOptions,"container"> =>{
         return {
-            waveColor: 'rgb(200, 0, 200)',
-            progressColor: 'rgb(100, 0, 100)',
+            waveColor: '#ccc',
+            progressColor: '#2d3c4d',
             url: `/api?audio=${fileName}`,
+            barWidth:3,
+            
         }
     },[]);
    
     const wavesurfer = useWavesurfer(containerRef, optionsMemo)
-
+// On play button click
+    const onPlayClick = useCallback(() => {
+        if(wavesurfer){
+            wavesurfer?.isPlaying() ? wavesurfer.pause() : wavesurfer.play()
+       
+        } }, [wavesurfer])
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0)
+    useEffect(() => {
+        if (!wavesurfer) return
+    
+        setCurrentTime(0)
+        setIsPlaying(false)
+    
+        const subscriptions = [
+          wavesurfer.on('play', () => setIsPlaying(true)),
+          wavesurfer.on('pause', () => setIsPlaying(false)),
+          wavesurfer.on('timeupdate', (currentTime) => setCurrentTime(currentTime)),
+        ]
+    
+        return () => {
+          subscriptions.forEach((unsub) => unsub())
+        }
+      }, [wavesurfer])
     return (
         <>
             <div ref={containerRef}
             ></div>
+            <Button color="success" onClick={()=>onPlayClick()}>{isPlaying ? `Pause-${currentTime}` : `Play`}</Button>
         </>
     );
 };
